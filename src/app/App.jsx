ded56390a2845d1ae42c996a26115d3ca12cba87
Informatica <informatica@alumno.local>
2026-03-25 11:57:48 -0600

    Commit 0

---
+++ src/app/App.jsx
@@ -0,0 +1,12 @@
+  const App = () => {
+
+  return (
+    <>
+      <h1 className="text-3xl">Hola mundo</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse earum expedita nulla, tenetur deserunt quia debitis veniam sunt eum soluta, alias a aperiam ut harum maiores tempore nostrum animi perferendis!</p>
+
+    </>
+  )
+}
+
+export default App
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tail'
+import tailwindcss from '@tail';
 
 // https://vite.dev/config/
 export default defineConfig({
